e3f409f1dbe8b3e35e0a7a8a31dd9ef0c4185431
Cherno Badjan <badjancherno@gmail.com>
2021-03-03 18:12:53 -0800

    endpoints

---
+++ data/favorites.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    yelp_id: 'Ys42wLKqrflqmtqkgqOXgA',
+    name: 'Luc Lac',
+    image_url: 'https://s3-media1.fl.yelpcdn.com/bphoto/azr6sD6VeJbdaiO2aKvSsw/o.jpg',
+    rating: '4.0',
+  },
+  {
+    yelp_id: 'OQ2oHkcWA8KNC1Lsvj1SBA',
+    name: 'Screen Door',
+    image_url: 'https://s3-media4.fl.yelpcdn.com/bphoto/lqmMYlLRV-aoH73koWA6Ew/o.jpg',
+    rating: '4.5',
+  },
+  {
+    yelp_id: '4ZjHsu_bNxPyPqSOq2fw4g',
+    name: 'Q Restaurant & Bar',
+    image_url: 'https://s3-media2.fl.yelpcdn.com/bphoto/jAH0XyZe5N8YTrOy71SuJg/o.jpg',
+    rating: '4.5',
+  }
+];
